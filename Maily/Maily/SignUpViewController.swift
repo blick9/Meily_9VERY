4add1e1b3e76be52b01689ca2f5c88a8f6547340
Koo Chanwoo <febrix9@gmail.com>
2017-03-30 18:54:45 +0900

    [9] Make Log-in & Sign Up View

---
+++ Maily/Maily/SignUpViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SignUpViewController.swift
+//  Maily
+//
+//  Created by Febrix on 2017. 3. 30..
+//  Copyright © 2017년 Febrix. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
