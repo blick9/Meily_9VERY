e61a904be2a6581175a591bccbfa9ec1bfb1629f
Koo Chanwoo <febrix9@gmail.com>
2017-03-30 18:14:35 +0900

    [K] Make Storyboard

--- Maily/Maily/LoginViewController.swift
+++ Maily/Maily/LoginViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
