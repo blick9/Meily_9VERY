f5ee42678e555e7cf56626004fd20fcc653471a4
nueola <ruddmsdl000@naver.com>
2017-04-03 20:15:17 +0900

    [B] Complete DatePicker

--- Maily/Maily/GoalAddViewController.swift
+++ Maily/Maily/GoalAddViewController.swift
@@ -9,31 +9,48 @@
 import UIKit
 
 class GoalAddViewController: UIViewController {
+    let dateFormatter = DateFormatter()
+    let datePicker = UIDatePicker()
 
+    @IBOutlet weak var datePickerTextField: UITextField!
     @IBOutlet weak var goalTitleTextField: UITextField!
     @IBOutlet weak var priority3Button: UIButton!
     @IBOutlet weak var priority2Button: UIButton!
     @IBOutlet weak var priority1Button: UIButton!
    
-   
     override func viewDidLoad() {
         super.viewDidLoad()
         setDatePicker()
-      
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    func setDatePicker()
-    {
-        var datePicker=UIDatePicker(frame: CGRect(x:107,y:375,width:230,height:38))
-        datePicker.datePickerMode = .date
-        datePicker.locale = Locale(identifier: "ko_KR")
-        datePicker.tintColor=UIColor.black
-        datePicker.backgroundColor=UIColor.white
-        self.view.addSubview(datePicker)
+    @IBAction func cancleButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func setDatePicker() {
+         let toolbar = UIToolbar()
+         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+         datePicker.datePickerMode = .date
+         datePicker.locale = Locale(identifier: "ko_KR")
+         toolbar.sizeToFit()
+         toolbar.setItems([doneButton], animated: false)
+         datePickerTextField.inputAccessoryView = toolbar
+         datePickerTextField.inputView = datePicker
+         datePickerTextField.setValue(UIColor.white, forKey:"textColor")
+    }
+    
+    func donePressed() {
+        let selectedYear = datePicker.date
+        dateFormatter.dateFormat = "YYYY년 MM월 dd일(EEEE)"
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+//        dateFormatter.dateStyle = .full
+//        dateFormatter.timeStyle = .none
+        datePickerTextField.text = dateFormatter.string(from: selectedYear)
+        self.view.endEditing(true)
     }
 
     /*
